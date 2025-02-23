64ea45ecf57d6ccdff3bce875876c05c36a75445
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-02-22 23:05:52 -0300

    feat: Testando a criação de editoras no banco de dados de duas formas

--- src/test/models/editora.test.js
+++ src/test/models/editora.test.js
@@ -1,4 +1,4 @@
-import { describe, expect } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 import Editora from '../../models/editora.js';
 
 describe('Testando o modelo Editora', () => {
@@ -13,6 +13,31 @@ describe('Testando o modelo Editora', () => {
 
     expect(editora).toEqual(
       expect.objectContaining(objEditora),
+    );
+  });
+
+  it.skip('Deve salvar editora no BD', () => {
+    const editora = new Editora(objEditora);
+
+    editora.salvar().then((dados) => {
+      expect(dados.nome).toBe('CDC');
+    });
+  });
+
+  it('Deve salvar no BD usando a sintaxe moderna', async () => {
+    const editora = new Editora(objEditora);
+
+    const dados = await editora.salvar();
+
+    const retornado = await Editora.pegarPeloId(dados.id);
+
+    expect(retornado).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      }),
     );
   });
 });
