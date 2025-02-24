2463f477cbdcc17aa727a0eaf6996c5902033d5d
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-02-24 20:45:38 -0300

    feat: Testantando as rotas de editora usando a biblioteca supertest

--- src/test/routes/editorasRoutes.test.js
+++ src/test/routes/editorasRoutes.test.js
@@ -1,4 +1,5 @@
-import { afterEach, beforeEach } from '@jest/globals';
+import { afterEach, beforeEach, describe, expect } from '@jest/globals';
+import request from 'supertest';
 import app from '../../app.js';
 
 let server;
@@ -10,4 +11,47 @@ beforeEach(() => {
 
 afterEach(() => {
   server.close();
+});
+
+describe('GET em /editoras', () => {
+  it('Deve retornar uma lista de editoras', async () => {
+    const resposta = await request(app)
+      .get('/editoras')
+      .set('Accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(200);
+
+    expect(resposta.body[0].email).toEqual('e@e.com');
+  });
+});
+
+let idResposta;
+describe('POST em /editoras', () => {
+  it('Deve adicionar uma nova editora', async () => {
+    const resposta = await request(app)
+      .post('/editoras')
+      .send({
+        nome: 'CDC',
+        cidade: 'São Paulo',
+        email: 's@s.com',
+      })
+      .expect(201);
+    idResposta = resposta.body.content.id;
+  });
+});
+
+describe('DELETE em /editoras/id', () => {
+  it('Deve deletar uma editora', async () => {
+    await request(app)
+      .delete(`/editoras/${idResposta}`)
+      .expect(200);
+  });
+});
+
+describe('GET em /editoras/id', () => {
+  it('Deve retornar uma editora selecionada', async () => {
+    await request(app)
+      .get(`/editoras/${idResposta}`)
+      .expect(200);
+  });
 });
