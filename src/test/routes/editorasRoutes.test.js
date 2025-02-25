84ff0de34335382fd0254d002f4ecd2d57854901
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-02-24 22:32:25 -0300

    feat: Testando e corrigindo outras partes do código de editoras

--- src/test/routes/editorasRoutes.test.js
+++ src/test/routes/editorasRoutes.test.js
@@ -1,4 +1,6 @@
-import { afterEach, beforeEach, describe, expect } from '@jest/globals';
+import {
+  afterEach, beforeEach, describe, expect,
+} from '@jest/globals';
 import request from 'supertest';
 import app from '../../app.js';
 
@@ -38,13 +40,12 @@ describe('POST em /editoras', () => {
       .expect(201);
     idResposta = resposta.body.content.id;
   });
-});
 
-describe('DELETE em /editoras/id', () => {
-  it('Deve deletar uma editora', async () => {
+  it('Deve não adicionar nada ao passar o body vazio', async () => {
     await request(app)
-      .delete(`/editoras/${idResposta}`)
-      .expect(200);
+      .post('/editoras')
+      .send({})
+      .expect(400);
   });
 });
 
@@ -52,6 +53,25 @@ describe('GET em /editoras/id', () => {
   it('Deve retornar uma editora selecionada', async () => {
     await request(app)
       .get(`/editoras/${idResposta}`)
+      .expect(200);
+  });
+});
+
+describe('PUT em /editoras/id', () => {
+  it('Deve alterar o campo nome', async () => {
+    await request(app)
+      .put(`/editoras/${idResposta}`)
+      .send({
+        nome: 'Casa do Código',
+      })
+      .expect(204);
+  });
+});
+
+describe('DELETE em /editoras/id', () => {
+  it('Deve deletar uma editora', async () => {
+    await request(app)
+      .delete(`/editoras/${idResposta}`)
       .expect(200);
   });
 });
